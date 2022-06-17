b27c1b3e68ea5cab3ab09beb7b39dcdbe6c3a1c6
Neko <83354761+xinyu-laura@users.noreply.github.com>
2022-06-18 05:54:30 +0800

    Update segformer_lawin_model.py

--- segformer_lawin_model.py
+++ segformer_lawin_model.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 #from .heads import SegFormerHead
 from seg_lawin_head import LawinHead
-import seg_mix
+import seg_mit
 
 
 segformer_settings = {
